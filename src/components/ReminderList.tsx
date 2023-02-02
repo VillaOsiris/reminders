ba7c8daa62f5205ea9e81d197144dac9ee390ecc
Sergio Costa <sergio20128@hotmail.com>
2023-02-02 16:35:28 +0000

    todo-app - typescript practice

---
+++ src/components/ReminderList.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Reminder from "../types/Reminder";
+
+interface ReminderListProps {
+  items: Reminder[];
+  onRemoveReminder: (id: number) => void;
+}
+
+export default function ReminderList({
+  items,
+  onRemoveReminder,
+}: ReminderListProps) {
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li key={item.id} className="list-group-item">
+          {item.title}
+          <button
+            onClick={() => onRemoveReminder(item.id)}
+            className="btn btn-outline-danger mx-2 rounded-pill"
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
